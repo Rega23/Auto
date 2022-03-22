66109f7a3e8213e38015482a2921b1ecb026567a
Rega23 <92116230+Rega23@users.noreply.github.com>
2022-03-22 18:09:33 +0700

    Update updatemenu.sh

--- update/updatemenu.sh
+++ update/updatemenu.sh
@@ -3,33 +3,24 @@
 # Color
 
 cd /usr/bin
-wget -O menu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/menu.sh"
-wget -O ipsaya "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/ipsaya.sh"
-wget -O sshovpnmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/sshovpn.sh"
-wget -O l2tpmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/l2tpmenu.sh"
-wget -O pptpmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/pptpmenu.sh"
-wget -O sstpmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/sstpmenu.sh"
-wget -O wgmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/wgmenu.sh"
-wget -O ssmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/ssmenu.sh"
-wget -O ssrmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/ssrmenu.sh"
-wget -O vmessmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/vmessmenu.sh"
-wget -O vlessmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/vlessmenu.sh"
-wget -O grpcmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/grpcmenu.sh"
-wget -O grpcupdate "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/grpcupdate.sh"
-wget -O trmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/trmenu.sh"
-wget -O trgomenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/trgomenu.sh"
-wget -O setmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/setmenu.sh"
-wget -O slowdnsmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/slowdnsmenu.sh"
-wget -O running "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/running.sh"
-wget -O updatemenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/updatemenu.sh"
+wget -O menu "https://raw.githubusercontent.com/Rega23/Auto/main/update/menu.sh"
+wget -O ipsaya "https://raw.githubusercontent.com/Rega23/Auto/main/update/ipsaya.sh"
+wget -O sshovpnmenu "https://raw.githubusercontent.com/Rega23/Auto/main/update/sshovpn.sh"
+wget -O ssmenu "https://raw.githubusercontent.com/Rega23/Auto/main/update/ssmenu.sh"
+wget -O ssrmenu "https://raw.githubusercontent.com/Rega23/Auto/main/update/ssrmenu.sh"
+wget -O vmessmenu "https://raw.githubusercontent.com/Rega23/Auto/main/update/vmessmenu.sh"
+wget -O vlessmenu "https://raw.githubusercontent.com/Rega23/Auto/main/update/vlessmenu.sh"
+wget -O grpcmenu "https://raw.githubusercontent.com/Rega23/Auto/main/update/grpcmenu.sh"
+wget -O grpcupdate "https://raw.githubusercontent.com/Rega23/Auto/main/update/grpcupdate.sh"
+wget -O trmenu "https://raw.githubusercontent.com/Rega23/Auto/main/update/trmenu.sh"
+wget -O trgomenu "https://raw.githubusercontent.com/Rega23/Auto/main/update/trgomenu.sh"
+wget -O setmenu "https://raw.githubusercontent.com/Rega23/Auto/main/update/setmenu.sh"
+wget -O running "https://raw.githubusercontent.com/Rega23/Auto/main/update/running.sh"
+wget -O updatemenu "https://raw.githubusercontent.com/Rega23/Auto/main/update/updatemenu.sh"
 
 chmod +x menu
 chmod +x ipsaya
 chmod +x sshovpnmenu
-chmod +x l2tpmenu
-chmod +x pptpmenu
-chmod +x sstpmenu
-chmod +x wgmenu
 chmod +x ssmenu
 chmod +x ssrmenu
 chmod +x vmessmenu
@@ -39,6 +30,5 @@ chmod +x grpcupdate
 chmod +x trmenu
 chmod +x trgomenu
 chmod +x setmenu
-chmod +x slowdnsmenu
 chmod +x running
 chmod +x updatemenu
