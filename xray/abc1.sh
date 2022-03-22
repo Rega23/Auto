4fe993508e60448a58c81a5b4dd0f0277a77f79e
Rega23 <92116230+Rega23@users.noreply.github.com>
2022-03-22 14:13:49 +0700

    Update abc1.sh

--- xray/abc1.sh
+++ xray/abc1.sh
@@ -6,10 +6,11 @@ domain=$(cat /etc/xray/domain)
 sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
 cd /root/
 wget https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
+acme.sh --set-default-ca --server letsencrypt
 bash acme.sh --install
 rm acme.sh
 cd .acme.sh
-bash acme.sh --register-account -m senowahyu62@gmail.com
+bash acme.sh --register-account -m rega.andriana@gmail.com
 bash acme.sh --issue --standalone -d $domain --force
 bash acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key
 
