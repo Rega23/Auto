9c78afb5768a2e2bbdc64a7113c7e3a8067e8725
Rega23 <92116230+Rega23@users.noreply.github.com>
2022-04-16 19:29:23 +0700

    Update menu.sh

--- update/menu.sh
+++ update/menu.sh
@@ -5,29 +5,24 @@ y="\033[0;1;37m"
 yy="\033[0;1;32m"
 yl="\033[0;1;33m"
 wh="\033[0m"
-echo -e "$y                  MENU UTAMA $wh"
 echo -e "$y-------------------------------------------------$wh"
-echo -e "$yy 1$y.  SSH & OpenVPN MENU  $wh"
-echo -e "$yy 2$y.  L2TP MENU$wh"
-echo -e "$yy 3$y.  PPTP MENU$wh"
-echo -e "$yy 4$y.  SSTP MENU$wh"
-echo -e "$yy 5$y.  WIREGUARD MENU$wh"
-echo -e "$yy 6$y.  SHADOWSOCKS MENU$wh"
-echo -e "$yy 7$y.  SHADOWSOCKSR MENU$wh"
-echo -e "$yy 8$y.  VMESS MENU$wh"
-echo -e "$yy 9$y.  VLESS MENU$wh"
-echo -e "$yy 10$y. TROJAN GFW MENU$wh"
-echo -e "$yy 11$y. TROJAN GO MENU$wh"
-echo -e "$yy 12$y. GRPC MENU$wh"
-echo -e "$yy 13$y. SLOWDNS MENU$wh"
-echo -e "$yy 14$y. CEK SEMUA IP PORT$wh"
-echo -e "$yy 15$y. CEK SEMUA SERVICE VPN$wh"
-echo -e "$yy 16$y. UPDATE MENU$wh"
-echo -e "$yy 17$y. SL Menu(Perbaiki Error SSLH+WS)$wh"
-echo -e "$yy 18$y. Settings$wh"
-echo -e "$yy 19$y. Exit$wh"
+echo -e "$y                MENU TUNNELING $wh"
 echo -e "$y-------------------------------------------------$wh"
-read -p "Select From Options [ 1 - 19 ] : " menu
+echo -e "$yy 1$y. SSH & OpenVPN MENU$wh"
+echo -e "$yy 2$y. VMESS MENU$wh"
+echo -e "$yy 3$y. VLESS MENU$wh"
+echo -e "$yy 4$y. TROJAN GFW MENU$wh"
+echo -e "$yy 5$y. TROJAN GO MENU$wh"
+echo -e "$yy 6$y. GRPC MENU$wh"
+echo -e "$y-------------------------------------------------$wh"
+echo -e "$y                  MENU SYSTEM $wh"
+echo -e "$y-------------------------------------------------$wh"
+echo -e "$yy 7$y. CEK SEMUA IP PORT$wh"
+echo -e "$yy 8$y. CEK SEMUA SERVICE VPN$wh"
+echo -e "$yy 9$y. Settings$wh"
+echo -e "$yy 10$y.Exit$wh"
+echo -e "$y-------------------------------------------------$wh"
+read -p "Select From Options [ 1 - 11 ] : " menu
 case $menu in
 1)
 clear
@@ -35,73 +30,37 @@ sshovpnmenu
 ;;
 2)
 clear
-l2tpmenu
+vmessmenu
 ;;
 3)
 clear
-pptpmenu
+vlessmenu
 ;;
 4)
 clear
-sstpmenu
+trmenu
 ;;
 5)
 clear
-wgmenu
+trgomenu
 ;;
 6)
 clear
-ssmenu
+grpcmenu
 ;;
 7)
 clear
-ssrmenu
+ipsaya
 ;;
 8)
 clear
-vmessmenu
+running
 ;;
 9)
 clear
-vlessmenu
-;;
-10)
-clear
-trmenu
-;;
-11)
-clear
-trgomenu
-;;
-12)
-clear
-grpcmenu
-;;
-13)
-clear
-slowdnsmenu
-;;
-14)
-clear
-ipsaya
-;;
-15)
-clear
-running
-;;
-16)
-clear
-updatemenu
-;;
-17)
-clear
-sl-fix
-;;
-18)
-clear
 setmenu
 ;;
-19)
+10)
 clear
 exit
 ;;
